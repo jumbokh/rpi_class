730c7b548d2b7aeb74ffcfadc6c66ef74d7352d0
jumbo Hsieh <jumbokh@gmail.com>
2019-12-05 10:38:53 +0800

    Add files via upload

--- test/gpio_bcm.py
+++ test/gpio_bcm.py
@@ -1,8 +1,14 @@
 import RPi.GPIO as GPIO
+from time import sleep
+
 # the same script as above but using BCM GPIO 00..nn numbers
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(18, GPIO.OUT)
-input_value = GPIO.input(17)
-GPIO.output(18, GPIO.HIGH)
+#GPIO.setup(2, GPIO.IN)
+GPIO.setup(2, GPIO.OUT)
+#input_value = GPIO.input(17)
+for i in range(5):
+    GPIO.output(2, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(2, GPIO.LOW)
+    sleep(1)
